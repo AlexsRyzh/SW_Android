a0c29f6c59d43685377ea83f8ac044c5c899a94a
Саша <alexsa>
2022-03-21 15:58:54 +0300

    Example

--- app/src/main/java/com/example/sw_android/MainActivity.kt
+++ app/src/main/java/com/example/sw_android/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.sw_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sw_android.ui.theme.SW_AndroidTheme
 
@@ -14,7 +19,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
+}
+
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message){
+    Column() {
+        for (i in 1..10){
+            Text("This number = $i")
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
