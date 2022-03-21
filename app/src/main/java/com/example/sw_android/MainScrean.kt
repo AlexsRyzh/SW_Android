a999f875a8f0f72dc13f3e7aa070fea4acbdee05
Professional <Max.14070@yandex.ru>
2022-03-21 19:44:18 +0300

    Hello

--- app/src/main/java/com/example/sw_android/MainScrean.kt
+++ app/src/main/java/com/example/sw_android/MainScrean.kt
@@ -1,13 +1,19 @@
 package com.example.sw_android
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import java.lang.reflect.Modifier
+import androidx.compose.ui.res.painterResource
+
 
 @Composable
 fun MainViev(){
@@ -16,11 +22,17 @@ fun MainViev(){
 
 @Composable
 fun Nav(name: String){
-    Row {
-       Button(
-           onClick = { /*TODO*/ },
-            ) {
-           
+    Row { Button(onClick = { /*TODO*/ }) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            drawLine(
+                start = Offset(x = canvasWidth, y = 0f),
+                end = Offset(x = 0f, y = canvasHeight),
+                color = Color.Blue
+            )
+        }
        }
         Column() {
             Text(text = name)
