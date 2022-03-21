3482b11414e11fcec8e9852b054e70ee4591ac57
Саша <alexsa>
2022-03-21 20:28:52 +0300

    Example2

--- app/src/main/java/com/example/sw_android/MainScrean.kt
+++ app/src/main/java/com/example/sw_android/MainScrean.kt
@@ -4,25 +4,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun MainViev() {
-    Nav(name = "Задачи")
+    Row(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ){
+        Nav(name = "Задачи")
+    }
+
 }
 
 @Composable
@@ -30,9 +37,11 @@ fun Nav(name: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .background(Color.White)
+            .padding(20.dp)
     ) {
         Button(
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Unspecified),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.),
             onClick = { /*TODO*/ },
         ) {
 
