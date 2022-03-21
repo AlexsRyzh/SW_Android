ea4d3362fedf4afeb4301d63d4e8a935bed765f6
Саша <alexsa>
2022-03-21 19:57:32 +0300

    Example2

--- app/src/main/java/com/example/sw_android/MainScrean.kt
+++ app/src/main/java/com/example/sw_android/MainScrean.kt
@@ -1,45 +1,66 @@
 package com.example.sw_android
 
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 
 
 @Composable
-fun MainViev(){
+fun MainViev() {
     Nav(name = "Задачи")
 }
 
 @Composable
-fun Nav(name: String){
-    Row { Button(onClick = { /*TODO*/ }) {
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            val canvasWidth = size.width
-            val canvasHeight = size.height
+fun Nav(name: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Button(
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Unspecified),
+            onClick = { /*TODO*/ },
+        ) {
 
-            drawLine(
-                start = Offset(x = canvasWidth, y = 0f),
-                end = Offset(x = 0f, y = canvasHeight),
-                color = Color.Blue
+        }
+
+        Column(
+
+        ) {
+            Text(
+                text = name,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Моё пространство",
+                textAlign = TextAlign.Center
             )
         }
-       }
-        Column() {
-            Text(text = name)
-            Text(text = "Моё пространство")
-        }
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
 
         }
+
     }
+}
+
+@Preview
+@Composable
+fun PrewivNav() {
+    Nav("Android")
 }
